49562c1d51c25293cdded25ac6e80f988fd67766
lolzorrior <joshuac@outlook.com>
2021-08-10 03:10:04 -0500

    MIT License added. RangedClassBookItem started.

---
+++ src/main/java/com/lolzorrior/supernaturalmod/items/RangedClassBookItem.java
@@ -0,0 +1,10 @@
+package com.lolzorrior.supernaturalmod.items;
+
+import net.minecraft.world.item.BookItem;
+
+public class RangedClassBookItem extends BookItem {
+    public RangedClassBookItem(Properties p_40643_) {
+        super(p_40643_);
+    }
+
+}
