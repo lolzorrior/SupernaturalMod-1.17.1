4c971ab14db5212710c8b318ce4213bb7d5a7fed
lolzorrior <joshuac@outlook.com>
2021-09-16 00:58:15 -0500

    Fixed class condition, which wasn't broken I was just in creative -_-

--- src/main/java/com/lolzorrior/supernaturalmod/util/SupernaturalClassCondition.java
+++ src/main/java/com/lolzorrior/supernaturalmod/util/SupernaturalClassCondition.java
@@ -3,6 +3,7 @@ package com.lolzorrior.supernaturalmod.util;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
+import com.lolzorrior.supernaturalmod.SupernaturalMod;
 import com.lolzorrior.supernaturalmod.capabilities.ISupernaturalClass;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -11,9 +12,12 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditions;
 import net.minecraftforge.common.util.LazyOptional;
 
+import javax.annotation.Nullable;
+
 import static com.lolzorrior.supernaturalmod.capabilities.SupernaturalClass.SCLASS;
 
 public class SupernaturalClassCondition implements LootItemCondition {
+    @Nullable
     final LootContext.EntityTarget enT;
     final String sclass;
 
@@ -24,16 +28,35 @@ public class SupernaturalClassCondition implements LootItemCondition {
 
     @Override
     public LootItemConditionType getType() {
-        return LootItemConditions.ENTITY_PROPERTIES;
+        return SupernaturalMod.SUPERNATURAL_HUMAN_CONDITION;
     }
 
     @Override
     public boolean test(LootContext lootContext) {
-        if(lootContext.getParam(this.enT.getParam()).getCapability(SCLASS) == null) {
-            return false;
-        }
         String cclass = lootContext.getParamOrNull(this.enT.getParam()).getCapability(SCLASS).orElseThrow(NullPointerException::new).getsClass();
         return cclass.equalsIgnoreCase(sclass);
+    }
+
+    public static SupernaturalClassCondition.Builder sClass(String string) {
+        return new SupernaturalClassCondition.Builder(string);
+    }
+
+    public static class Builder implements LootItemCondition.Builder {
+        @Nullable
+        private LootContext.EntityTarget enT;
+        private final String sClass;
+
+        public Builder(String string) {
+            this.sClass = string;
+        }
+
+        public SupernaturalClassCondition.Builder entityType(LootContext.EntityTarget entIn){
+            this.enT = entIn;
+            return this;
+        }
+        public SupernaturalClassCondition build() {
+            return new SupernaturalClassCondition(this.enT, this.sClass);
+        }
     }
 
 
